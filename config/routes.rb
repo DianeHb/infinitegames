c2fcdaaeed8bc84a36ef00b890022a038c7c6275
Diane Hubau <diane.hubau@gmail.com>
2020-12-01 17:12:55 +0100

    Add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :library_games, only: [:index, :show] do
+    member do
+      get :lend
+      patch :lend
+    end
+    resources :game_sessions, only: [:index, :create]
+  end
+
+  resource :searches, only: [:show]
+
+  resources :games, only: [:index, :show] do
+    resources :reviews, only: [:create]
+  end
+
+  resources :friends, only: [:index, :create]
 end
