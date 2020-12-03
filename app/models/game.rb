7aeff34494556c82c8af5ef83fb206160dc5c542
devautour julien <warnacraft@hotmail.fr>
2020-12-03 08:28:38 +0100

    Display all games + change color and font

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,11 +7,11 @@ class Game < ApplicationRecord
   has_many :library_games
   has_many :games_sessions
 
-  validates :name, presence :true
-  validates :description, presence :true
-  validates :min_players, presence :true
-  validates :max_players, presence :true
-  validates :min_playtime, presence :true
-  validates :max_playtime, presence :true
+  # validates :name, presence :true
+  # validates :description, presence :true
+  # validates :min_players, presence :true
+  # validates :max_players, presence :true
+  # validates :min_playtime, presence :true
+  # validates :max_playtime, presence :true
 
 end
