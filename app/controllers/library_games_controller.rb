c11f1ce2c7fdcc9272c17f7b3ccd00ca1d8a343e
Diane Hubau <diane.hubau@gmail.com>
2020-12-04 10:06:26 +0100

    Change Route & Def Create for LbGames

--- app/controllers/library_games_controller.rb
+++ app/controllers/library_games_controller.rb
@@ -1,5 +1,20 @@
 class LibraryGamesController < ApplicationController
   def create
-
+    @library_game = LibraryGame.new(library_game_params)
+    @game = Game.find(params[:game_id])
+    @library_game.game = @game
+    @library_game.owner = current_user
+    if @library_game.save
+      redirect_to library_game_path(@library_game)
+    else
+      #RENDER THE MESSAGE
+    end
   end
+
+  private
+
+  def library_game_params
+    params.require(:library_game).permit()
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :library_games, only: [:index, :show, :create] do
+  resources :library_games, only: [:index, :show] do
     collection do
       get :lent
     end
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   resources :games, only: [:index, :show] do
     resources :reviews, only: [:create]
+    resources :library_games, only: [:create]
   end
 
   resources :friends, only: [:index, :create]
