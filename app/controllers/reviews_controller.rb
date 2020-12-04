20d8d288f72260ea42cd6236b97a1d04cb21b9e2
Diane Hubau <diane.hubau@gmail.com>
2020-12-04 16:30:39 +0100

    Fix

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,8 +3,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @library_game = LibraryGame(params[:id])
-    if
-      @review.save
+    if @review.save
       redirect_to @library_game , notice: 'review was successfully created.'
     else
       # render :show ???
@@ -14,6 +13,6 @@ class ReviewsController < ApplicationController
 
 private
   def review_params
-    params.require(:review.permit(:rating, :text)
+    params.require(:review).permit(:rating, :text)
   end
 end
