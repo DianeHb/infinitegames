057d954742ac6b9bc0bb01ad81ec0583e8d2da9b
Diane Hubau <diane.hubau@gmail.com>
2020-12-04 10:27:11 +0100

    Change Def Create

--- app/controllers/library_games_controller.rb
+++ app/controllers/library_games_controller.rb
@@ -1,20 +1,15 @@
 class LibraryGamesController < ApplicationController
   def create
-    @library_game = LibraryGame.new(library_game_params)
     @game = Game.find(params[:game_id])
-    @library_game.game = @game
-    @library_game.owner = current_user
+    @library_game = LibraryGame.new(
+      @library_game.game = @game
+      @library_game.owner = current_user
+    )
     if @library_game.save
       redirect_to library_game_path(@library_game)
     else
-      #RENDER THE MESSAGE
+      #RENDER AN ERROR MESSAGE ?
     end
-  end
-
-  private
-
-  def library_game_params
-    params.require(:library_game).permit()
   end
 
 end
