deea309f0336c18a098c5e693f9a69646a02298b
Diane Hubau <diane.hubau@gmail.com>
2020-12-07 09:53:36 +0100

    Create library

--- app/controllers/library_games_controller.rb
+++ app/controllers/library_games_controller.rb
@@ -2,20 +2,17 @@ class LibraryGamesController < ApplicationController
   def index
     @games = current_user.library_games
   end
-  
+
   def create
     @game = Game.find(params[:game_id])
     @library_game = LibraryGame.new(
       game: @game,
       owner: current_user
     )
-
     if @library_game.save
-      redirect_to game_path(@game)
-      # redirect_to library_game_path(@library_game)
+      redirect_to library_game_path(@library_game)
     else
-      redirect_to root_path
-      #RENDER AN ERROR MESSAGE ?
+      redirect_to library_games_path
     end
   end
 
