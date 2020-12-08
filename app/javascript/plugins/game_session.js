c280d588f07a2d0b7ed99fb6b430184c096114b6
Diane Hubau <diane.hubau@gmail.com>
2020-12-08 16:19:36 +0100

    JS to change the type of player, linked to abyme

--- app/javascript/plugins/game_session.js
+++ app/javascript/plugins/game_session.js
@@ -1,28 +1,23 @@
 const initGameSession = () => {
-
   //document.getElementById('abyme--tasks').addEventListener('abyme:after-add', yourCallback)
-  document.getElementById('abyme--tasks').addEventListener('abyme:after-add', (event) => {
+  document.getElementById('abyme--game_session_players').addEventListener('abyme:after-add', (event) => {
     let all_players = document.querySelectorAll('.selected_player');
+    all_players.forEach((element) => {
+      let player = element["children"][0]["children"][1]
+      //document.getElementById("game_session_game_session_players_attributes_1607437847841_player_id");
+      //id="game_session_game_session_players_attributes_1607437847841_player_id"
+      let playerType = element["children"][1]
+      //document.getElementById("game_session_game_session_players_attributes_1607437847841_player_type");
+      //id="game_session_game_session_players_attributes_1607437847841_player_type"
+      if (player) {
+        player.addEventListener('change', (event) => {
+          let group = player.options[player.selectedIndex].parentNode.label;
+          playerType.value = group == "Guests" ? "Guest" : "User";
+          console.log(playerType.value)
+        });
+      }
+    });
   });
-
-  all_players.forEach((element) => {
-    let player = element["children"][0]["children"][1]
-    //document.getElementById("game_session_game_session_players_attributes_1607437847841_player_id");
-    //id="game_session_game_session_players_attributes_1607437847841_player_id"
-    let playerType = element["children"][1]
-    //document.getElementById("game_session_game_session_players_attributes_1607437847841_player_type");
-    //id="game_session_game_session_players_attributes_1607437847841_player_type"
-    if (player) {
-      player.addEventListener('change', (event) => {
-        let group = player.options[player.selectedIndex].parentNode.label;
-        console.log(group)
-        // let playerType.value = group == "Guests" ? "Guest" : "User";
-        console.log(playerType)
-      });
-    }
-  });
-
-
 };
 
 export {initGameSession}
