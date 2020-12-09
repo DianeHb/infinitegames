82ff54bd5178603cf16a39780835aa11ef46a487
n.l <n.leteurtre@gmail.com>
2020-12-09 11:42:47 +0100

    add banner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def game_short_description(game)
     sanitize(game.description)[0..160]+"..."
   end
+
+  def game_short_description_library(game)
+    sanitize(@game.game.description)[0..700]+"..."
+  end
 end
